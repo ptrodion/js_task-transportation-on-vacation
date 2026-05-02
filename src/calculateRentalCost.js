a6026c9cdd1a1752e113766edd043286f60e48d4
ptrodion <rdnpetrenko@gmail.com>
2026-05-02 10:55:52 +0100

    Final decision

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,17 @@
  *
  * @return {number}
  */
+
+const dailyRate = 40;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days >= 7) {
+    return dailyRate * days - 50;
+  } else if (days >= 3 && days < 7) {
+    return dailyRate * days - 20;
+  } else {
+    return dailyRate * days;
+  }
 }
 
 module.exports = calculateRentalCost;
